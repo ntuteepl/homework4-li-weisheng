3de51524626e21307c4ef8ffe9c7ff3c3577952b
112310204 <146065554+li-weisheng@users.noreply.github.com>
2023-10-29 00:10:46 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+
+int isPrime(int num) {
+    if (num <= 1) {
+        return 0;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return 0; // 如果能被小于等于其平方根的数整除，不是质数
+        }
+    }
+    return 1; // 是质数
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        int num;
+        scanf("%d", &num);
+        if (isPrime(num)) {
+            printf("YES ");
+        } else {
+            printf("NO ");
+        }
+    }
+
+    return 0;
+}
 
