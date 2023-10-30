f34fd9d4c48de6e03c7f4e582b4590974ce5cec3
112310204 <146065554+li-weisheng@users.noreply.github.com>
2023-10-31 00:56:35 +0800

    Update hw4-4.c

--- hw4-4.c
+++ hw4-4.c
@@ -1,72 +1,65 @@
-#define ARRAYSIZE 100
-
-#include <stdio.h>
+/*#include <stdio.h>
 
 int main() {
-    int n = 0;
-    int sd[ARRAYSIZE][2];
-    int t[ARRAYSIZE][2];
-    int cars = 0; // 初始化为0
-    while (scanf("%d %d", &sd[n][0], &sd[n][1]) != EOF) {
-        n++;
-    }
-
-    // 冒泡排序
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (sd[j][0] > sd[j + 1][0] || (sd[j][0] == sd[j + 1][0] && sd[j][1] > sd[j + 1][1])) {
-                // Swap the rows if the first column element is out of order
-                int temp1 = sd[j][0];
-                int temp2 = sd[j][1];
-                sd[j][0] = sd[j + 1][0];
-                sd[j][1] = sd[j + 1][1];
-                sd[j + 1][0] = temp1;
-                sd[j + 1][1] = temp2;
-            }
-        }
-    }
-     for (int i = 0; i < n; i++)
-     {
-         t[i][0]=sd[i][0];
-         t[i][1]=sd[i][1];
-     }
-
-
+    int n;
+    scanf("%d", &n);
+    int preferences[n-1][n-1];
+    int boy_best_match[n-1], girl_best_match[n-1];
     for (int i = 0; i < n; i++) {
-        if (sd[i][0] != 0) { // Skip already processed rows
-            cars++;
-            int current_time = sd[i][1]; // 当前司机的时间表
-
-            for (int j = i + 1; j < n; j++) {
-                if (sd[j][0] >= current_time) {
-                    // 当前任务可以分配给当前司机
-                    current_time = sd[j][1];
-                    sd[j][0] = 0; // 将任务标记为已分配
-                }
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &preferences[i][j]);
+        }
+    }
+    int boy, girl,k,i,j;
+    int max_feeling;
+    for (k = 0; k < preferences; k++) {
+        max_feeling = -1;
+        for (int l = 0; l < preferences; l++) {
+            if (preferences[i][j] > max_feeling) {
+                max_feeling = preferences[i][j];
+                girl = l;
             }
+        }
+        // 女孩被匹配後，將其好感度設為-1，以避免重複匹配
+        preferences[i][girl] = -1;
+        printf("boy %d pair with girl %d.\n", i + 1, girl + 1);
+    }
+
+    return 0;
+}*/
+#include <stdio.h>
+int main() {
+    int MAX_USERS;
+    scanf("%d", &MAX_USERS);
+    int preferences[MAX_USERS-1][MAX_USERS-1]; // 好感度矩陣
+    int i, j,n;
+
+    // 使用者輸入好感度矩陣
+
+    for (i = 0; i < MAX_USERS; i++) {
+        for (j = 0; j < MAX_USERS; j++) {
+            scanf("%d", &preferences[i][j]);
         }
     }
 
-   printf("%d\n", cars);
-   cars=0;
-    for (int i = 0; i < n; i++) {
-        if (t[i][0] != 0) { // Skip already processed rows
-            cars++;
-            printf("Driver %d's schedule is %d %d ", cars, t[i][0], t[i][1]);
+    int boy, girl;
+    int max_feeling;
 
-            for (int j = i + 1; j < n; j++) {
-                if (t[j][0] >= t[i][1]) {
-                    // 当前任务可以分配给当前司机
-                    printf("%d %d ", t[j][0], t[j][1]);
-                    t[i][1] = t[j][1];
-                    t[j][0]=0; // 更新当前司机的结束时间
-                }
+    // 找出最佳情侶配對
+    for (i = 0; i < MAX_USERS; i++) {
+        max_feeling = -1;
+        for (j = 0; j < MAX_USERS; j++) {
+            if (preferences[i][j] > max_feeling) {
+                max_feeling = preferences[i][j];
+                girl = j;
             }
-            printf("\n");
-     
         }
+        // 女孩被匹配後，將其好感度設為-1，以避免重複匹配
+        preferences[i][girl] = -1;
+        printf("boy %d pair with girl %d.\n", i + 1, girl + 1);
     }
 
     return 0;
 }
+
 
